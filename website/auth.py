ac6eb42ac185e66f925f776019cdd8a26c8bea47
cmoore844 <95652064+cmoore844@users.noreply.github.com>
2022-07-19 14:34:45 -0400

    Progress 7/19
    Working on resolving jinja2 exception error

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,16 @@
+#defines the login and authentication services used for website
 from flask import Blueprint
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("hello")
-def function():
-    return "hello"
+@auth.route("/login")
+def login():
+    return "<p>Login</p>"
+
+@auth.route("/logout")
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route("/Sign-up")
+def Sign_up():
+    return "<p>Sign-Up</p>"
